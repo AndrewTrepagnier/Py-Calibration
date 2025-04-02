cff755eaf9c9e5893efe6dd46f451ab5cd66cfa2
AndrewTrepagnier <andrew.trepagnier1@gmail.com>
2025-04-02 11:38:15 -0500

    Pure Y test successful, next attempt multiple dump files

--- NN-calibration-multi-thread/Test-case-Y/TestY.py
+++ NN-calibration-multi-thread/Test-case-Y/TestY.py
@@ -10,8 +10,9 @@ from fingerprint_radial import Fingerprint_radial
 Edit the json file to test different filepaths or parameters. We can manually input the parameters we know our fingerprint should be producing 
 and run it through this testing script to see if the fingerprint is computing and parsing the way we desire.
 """
-
-def load_test_config(config_files = "test_config1.json"):
+# Change name of the configuration file as needed. Sine we are testing Yttrium in this instance, we use TestY.json
+# The configuration json file should be located in the same directory as this test script, TestY.py
+def load_test_config(config_files = "TestY.json"): 
     """Loads the test configuration from the json file"""
     with open(config_files, 'r') as f:
         return json.load(f)
@@ -21,43 +22,43 @@ def make_fingerprint_instance():
     # load test configuration
     config = load_test_config()
     #initialize fingerprint calculator
-    Zn_fingerprint = Fingerprint_radial(inputpath=config['input_file'],
+    Y_fingerprint = Fingerprint_radial(inputpath=config['input_file'],
                                       dumppath=config['dump_file'])
-    return Zn_fingerprint, config
+    return Y_fingerprint, config
 
 
 
 def test_input_parser():
-    Zn_fingerprint, config = make_fingerprint_instance()
+    Y_fingerprint, config = make_fingerprint_instance()
 
-    Zn_fingerprint.input_parser()
-    assert abs(Zn_fingerprint.params.rc - config['rc'])<1e-4, \
-    f"cutoff radius {Zn_fingerprint.params.rc} doesn't match expected {config['rc']}"
+    Y_fingerprint.input_parser()
+    assert abs(Y_fingerprint.params.rc - config['rc'])<1e-4, \
+    f"cutoff radius {Y_fingerprint.params.rc} doesn't match expected {config['rc']}"
 
 
 
 def test_output_parser():
-    Zn_fingerprint, config = make_fingerprint_instance()
+    Y_fingerprint, config = make_fingerprint_instance()
 
-    Zn_fingerprint.dump_parser()
+    Y_fingerprint.dump_parser()
     # Can we save our data in system
-    assert len(Zn_fingerprint.systems) > 0, "No systems loaded"
+    assert len(Y_fingerprint.systems) > 0, "No systems loaded"
     # What is the number of atoms that was parsed
-    assert Zn_fingerprint.systems[0].num_atoms == config['num_atoms'], \
-    f"Number of atoms {Zn_fingerprint.systems[0].num_atoms} doesn't match the expected to be {config['num_atoms']}"
+    assert Y_fingerprint.systems[0].num_atoms == config['num_atoms'], \
+    f"Number of atoms {Y_fingerprint.systems[0].num_atoms} doesn't match the expected to be {config['num_atoms']}"
     # What is the energy
-    assert abs(Zn_fingerprint.systems[0].energy - config['energy']) < 1e-4, \
-    f"The energy parsed in the dump file, {Zn_fingerprint.systems[0].energy} doesn't match the {config['energy']}"
+    assert abs(Y_fingerprint.systems[0].energy - config['energy']) < 1e-4, \
+    f"The energy parsed in the dump file, {Y_fingerprint.systems[0].energy} doesn't match the {config['energy']}"
 
 
 
 def test_distance():
-    Zn_fingerprint, config = make_fingerprint_instance()
+    Y_fingerprint, config = make_fingerprint_instance()
     
-    Zn_fingerprint.dump_parser()
+    Y_fingerprint.dump_parser()
     atom1 = config['test_distance']['atom1']
     atom2 = config['test_distance']['atom2']
-    computed_distance = Zn_fingerprint.systems[0].distance_matrix[atom1, atom2] # computes the euclidian distance between atom 0 and atom 1 in the matrix we store their coordinate positions in
+    computed_distance = Y_fingerprint.systems[0].distance_matrix[atom1, atom2] # computes the euclidian distance between atom 0 and atom 1 in the matrix we store their coordinate positions in
     expected_distance = config['test_distance']['expected_distance'] # calculated the desired by hand, but an equation could be inserted here if need be
     
     assert abs(computed_distance - expected_distance) < 1e-4, \
@@ -65,11 +66,11 @@ def test_distance():
         f"but expected {expected_distance}"
     
 def call_fingerprint_computation():
-    Zn_fingerprint, config = make_fingerprint_instance()
-    Zn_fingerprint.input_parser()
-    Zn_fingerprint.dump_parser()
-    Zn_fingerprint.radii_table()
-    Zn_fingerprint.compute_fingerprint()
+    Y_fingerprint, config = make_fingerprint_instance()
+    Y_fingerprint.input_parser()
+    Y_fingerprint.dump_parser()
+    Y_fingerprint.radii_table()
+    Y_fingerprint.compute_fingerprint()
 
 
 if __name__ == "__main__":
